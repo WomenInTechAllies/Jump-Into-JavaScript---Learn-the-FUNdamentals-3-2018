3f15ab2004c3508a7728637570a17ea2c27c3a6c
Susan Wands <susanwands@users.noreply.github.com>
2018-03-10 15:17:33 -0500

    Rainbow Project

---
+++ main.js
@@ -0,0 +1,9 @@
+alert("Susan says to do this");
+const btn = document.getElementById('button');
+
+const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+
+function change() {
+  document.body.style.background = rainbow[Math.floor(7*Math.random())];
+}
+btn.addEventListener('click', change);
